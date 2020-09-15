f181a872ac1f05b58d6b6b7da0a1554f7930364b
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-09-15 23:35:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,10 @@ def oxford_comma(array)
   if array.length >= 2
     element = array[-1]
     array[-1] = "and #{element}"
+  else array.length = 2 
+    element1 = array[0]
+    element2 = array[1]
+    "#{element1} and #{element2}"
   end
   array.join(", ")
 end
