3d4840a2c7612a5c1a5f66e040a3c70f05eca9db
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-09-15 23:52:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,6 @@ def oxford_comma(array)
   if array.length <= 1 
     array.join
   elsif array.length > 2
-    array.join(",")
     element = array[-1]
     array[-1] = "and #{element}"
     array.join(", ")
